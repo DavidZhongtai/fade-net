fe6b9f4dbb754cf8000b27b2204c03459646c8fd
David An <davidzhongtaian@gmail.com>
2021-01-06 21:08:58 -0500

    update commit

---
+++ process.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+def clean_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+#Removing the square brackets
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+    
+# Removing URL's
+def remove_urls (text):
+    return re.sub(r'http\S+', '', text)
